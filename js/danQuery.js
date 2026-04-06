c3d8e497a038297691a0b4c92570ca9dd0f9c6d5
Dan Rollans <danrollans@yahoo.com>
2026-04-06 15:47:44 -0700

    svg magic

--- js/danQuery.js
+++ js/danQuery.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
 
 let cl = console.log.bind(console);
+let path;
+let length;
+
+for(let i=1;i<=11;i++){
+    cl("hello")
+    path = document.querySelector('#svg-path-'+i);
+    length = path.getTotalLength();
+    cl('path length= ' + length);
+};
+
 
 // stars stuff
+/*
 var screenWid = $(window).width() - 50;
 var randStartPos;
 var randEndPos;
@@ -33,43 +44,7 @@ $('.card').hover(function(){
 
 });
 
-// for (i = 1; i <= 100; i++) {
-//     randStartPos = Math.floor(Math.random() * screenWid) + 20;
-//     randEndPos = Math.floor(Math.random() * screenWid) + 20;
-//     randStarSize = Math.floor(Math.random() * 8) + 1;
-//     aniDuration = Math.floor(Math.random() * 37000) + 28000;
-//     randAniDelay = Math.random() * 21000 + .5000;
-//     randAniDelayCirc = Math.floor(Math.random() * 3280) + 200;
-//     randCircColor = Math.floor(Math.random() * 4);
-
-//     $('.container').append('<div class="circle-container"><div class="circle"></div>');
-
-//     $('.circle-container:nth-child(' + i + ')').css({
-//         'z-index': -10,
-//         'width': randStarSize,
-//         'height': randStarSize,
-//         'transform': 'translate3d(' + randStartPos + 'px, -10px, 0)',
-//         '-webkit-animation-name': 'move-frames-' + i,
-//         'animation-name': ' move-frames-' + i,
-//         '-webkit-animation-duration': aniDuration + 'ms',
-//         'animation-duration': aniDuration,
-//         '-webkit-animation-delay': '.' + randAniDelay + 'ms',
-//         'animation-delay': randAniDelay + 'ms',
-//     });
-
-//     $('.circle-container:nth-child(' + i + ') .circle').css({
-//         '-webkit-animation-delay': randAniDelayCirc + 'ms',
-//         'animation-delay': randAniDelayCirc + 'ms',
-//         'background-image': 'radial-gradient(' + randHexColorAr[randCircColor] + ',' + randHexColorAr[randCircColor] + ' 10%, rgba(' + randRbgColorAr[randCircColor][0] + ', ' + randRbgColorAr[randCircColor][1] + ', ' + randRbgColorAr[randCircColor][2] + ', 0) 56%)'
-
-//     });
-
-//     var keyFrames = '@keyframes move-frames-' + i + '{from {-webkit-transform: translate3d(' + randStartPos + 'px, 100px, 0)transform: translate3d(' + randStartPos + 'px, 100px, 0);}to {-webkit-transform: translate3d(' + randEndPos + 'px, 500px, 0);transform: translate3d(' + randEndPos + 'px, 500px, 0);}}@-webkit-keyframes move-frames-' + i + '{from {-webkit-transform: translate3d(' + randStartPos + 'px, 100px, 0)transform: translate3d(' + randStartPos + 'px, 100px, 0);}to {-webkit-transform: translate3d(' + randEndPos + 'px, 500px, 0);transform: translate3d(' + randEndPos + 'px, 500px, 0);}}';
-
-//     style.innerHTML += keyFrames;
-//     document.getElementsByTagName('head')[0].appendChild(style);
-
 
 //
-
+*/
 });
